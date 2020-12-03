b7369f5e480e09655eea61a006f6f49c67b2abca
hlavatskyi.serhii <hlavatskyi.serhii@steelkiwi.com>
2020-12-03 19:49:27 +0200

    disabled csrf

--- ofam_telegram/views.py
+++ ofam_telegram/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
