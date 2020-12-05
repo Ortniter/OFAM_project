d27c190473333615ccef5f2d8c5e04a4983e6f65
hlavatskyi.serhii <hlavatskyi.serhii@steelkiwi.com>
2020-12-05 19:51:44 +0200

    parser for ofam_shop started

--- ofam_telegram/ofam_scraper.py
+++ ofam_telegram/ofam_scraper.py
@@ -1,15 +1,43 @@
 from pyvirtualdisplay import Display
 from selenium import webdriver
+from time import sleep
+from bs4 import BeautifulSoup
+import requests
 
 with Display():
-    # we can now start Firefox and it will run inside the virtual display
-    browser = webdriver.Firefox()
+    driver = webdriver.Firefox()
 
-    # put the rest of our selenium code in a try/finally
-    # to make sure we always clean up at the end
     try:
-        browser.get('http://www.google.com')
-        print(browser.title) #this should print "Google"
+        driver.get("https://www.ofam.org.ua/ua/shop")
+        driver.find_element_by_css_selector('.js-store-load-more-btn > table:nth-child(1)').click()
+        sleep(10)
+
+        item_divs = driver.find_elements_by_class_name('js-product')
+        for item_div in item_divs:
+            item_div.click()
+            sleep(5)
+            url = driver.current_url
+
+            html = requests.get(url).text
+
+            soup = BeautifulSoup(html, 'lxml')
+
+            name = soup.find('div', class_='js-store-prod-name').text.strip()
+            description = soup.find('div', class_='js-store-prod-text').text.strip()
+            price = soup.find('div', class_='js-product-price').text.strip()
+            image_url = driver.find_element_by_class_name('t-slds__imgwrapper').get_attribute('data-img-zoom-url')
+
+            print(name)
+            print(description)
+            print(price)
+            print(image_url)
+            print(url)
+
+            sleep(2)
+            close_button = driver.find_element_by_class_name('js-store-close-text')
+            close_button.click()
+            sleep(2)
 
     finally:
-        browser.quit()
+        driver.close()
+        driver.quit()
