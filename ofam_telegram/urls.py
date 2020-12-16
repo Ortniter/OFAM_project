ae7c1498b55db10dea3f1a8e149cf3a54592a18f
hlavatskyi.serhii <hlavatskyi.serhii@steelkiwi.com>
2020-12-16 11:06:53 +0200

    new bot token

--- ofam_telegram/urls.py
+++ ofam_telegram/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('1270547075:AAHb_afunhhW2CWQrZ84NWYCbvHVz-AoYNk/', views.index, name='index'),
+    path('1207325258:AAHnQUKZH82bX8IWrcG2UGrV43JNprOZEYA/', views.index, name='index'),
     path('update_shop/', views.update_shop, name='update_shop')
 ]
